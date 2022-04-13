0dc8b293431f496616424080f2428821a521ed54
Mark Nagibin <myrkwill@icloud.com>
2022-04-13 15:33:28 +0300

    Add new components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './Profile.css'
+
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='https://i1.wp.com/static.web-backgrounds.net/uploads/2012/08/City_Landscape_Background.jpg' />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+			</div>
+			<div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+	);
+}
+
+export default Profile;
