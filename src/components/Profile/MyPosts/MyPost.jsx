87f3bc9dbd216a517b9667316063760d6f9396f9
Mark Nagibin <myrkwill@icloud.com>
2022-04-29 00:09:01 +0300

    Remove unused import

--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/profileReducer";
 
 const MyPosts = props => {
 	let postsElements = props.posts.map(p => (
