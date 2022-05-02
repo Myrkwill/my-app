64fd07c4d3fb1c211d189e2e7731e43f7ee855cf
Mark Nagibin <myrkwill@icloud.com>
2022-05-03 00:19:49 +0300

    Setup basename

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ let AppContainer = connect(mapStateToProps, { initializeApp })(App);
 
 
 const SamuraiJSApp = (props) => {
-  return <BrowserRouter>
+  return <BrowserRouter basename={procces.env.PUBLIC_URL}>
     <Provider store={store}>
       <AppContainer />
     </Provider>
